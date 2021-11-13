c543cbcd71e3af1d79bb5c0650e285f77d32b923
web-novice-tech <itb4769we@gmail.com>
2021-11-13 18:23:45 +0900

    list(hello) == ['h', 'e', 'l', 'l', 'o']

---
+++ ABC/083_b.py
@@ -0,0 +1,7 @@
+# https://atcoder.jp/contests/abc083/tasks/abc083_b
+N, A, B = map(int, input().split())
+ans = 0
+for i in range(N+1):
+    if A <= sum(list(map(int, list(str(i))))) <= B:
+        ans += i
+print(ans)
