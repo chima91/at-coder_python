8deb3fae25d58c12c3f290cc8a0275da7a7d1001
web-novice-tech <itb4769we@gmail.com>
2021-11-13 17:58:54 +0900

    制約が小さいため普通のfor文を回す。

---
+++ ABC/087_b.py
@@ -0,0 +1,10 @@
+# https://atcoder.jp/contests/abc087/tasks/abc087_b
+
+A, B, C, X = map(int, [input() for _ in range(4)])
+ans = 0
+for i in range(A + 1):
+    for j in range(B + 1):
+        for k in range(C + 1):
+            if (i*500 + j*100 + k*50) == X:
+                ans += 1
+print(ans)
