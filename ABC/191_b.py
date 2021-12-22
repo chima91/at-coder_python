908b8cfd7b97914f741fc3844daf9c1d68b3b98e
web-novice-tech <itb4769we@gmail.com>
2021-12-22 09:36:24 +0900

    リスト内包表記を使った解き方

--- ABC/191_b.py
+++ ABC/191_b.py
@@ -1,9 +1,4 @@
 N, X = map(int, input().split())
 A = list(map(int, input().split()))
 
-ans = []
-for i in range(N):
-    if A[i] != X:
-        ans.append(A[i])
-
-print(*ans)
+print(" ".join([str(A[i]) for i in range(N) if A[i] != X]))
