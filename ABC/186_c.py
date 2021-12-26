115989dcf95f737b3dea0186c5f452af57c404c0
web-novice-tech <itb4769we@gmail.com>
2021-12-26 22:21:58 +0900

    10進数を8進数に変換する場合、8で割った余りを一番上の桁にする→8で割った商に置き換える、という操作を0になるまで繰り返す。

--- ABC/186_c.py
+++ ABC/186_c.py
@@ -1,7 +1,24 @@
+def judge_ten(x):
+    x = str(x)
+    if '7' in x:
+        return True
+    else:
+        return False
+
+
+def judge_eight(x):
+    s = ''
+    while x > 0:
+        s = str(x % 8) + s
+        x //= 8
+    return True if '7' in s else False
+
+
 N = int(input())
 
+ans = 0
 for i in range(1, N+1):
-    if ('7' in str(i)) or ('7' in oct(i)):
-        N -= 1
+    if judge_ten(i) == False and judge_eight(i) == False:
+        ans += 1
 
-print(N)
+print(ans)
