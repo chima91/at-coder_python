81dc4b22a743308feb2dc902d5f75d191a551746
web-novice-tech <itb4769we@gmail.com>
2022-01-15 20:58:04 +0900

    文字列を先頭に加える、という操作は文字列の長さをNとして計算量がO(N)かかってしまう。dequeを使うとこれがO(1)でできる。

---
+++ ABC/158_d.py
@@ -0,0 +1,42 @@
+# https://atcoder.jp/contests/abc158/tasks/abc158_d
+from collections import deque
+S = input()
+Q = int(input())
+
+inv = False
+
+S_deque = deque()
+
+for i in range(len(S)):
+    S_deque.append(S[i])
+
+for i in range(Q):
+    TFC = list(map(str, input().split()))
+
+    if TFC[0] == "1":
+        if inv == False:
+            inv = True
+        else:
+            inv = False
+
+    else:
+        F = TFC[1]
+        C = TFC[2]
+
+        if inv == False:
+            if F == "1":
+                S_deque.appendleft(C)
+            else:
+                S_deque.append(C)
+        else:
+            if F == "1":
+                S_deque.append(C)
+            else:
+                S_deque.appendleft(C)
+
+ans = "".join(S_deque)
+
+if inv == True:
+    ans = ans[::-1]
+
+print(ans)
