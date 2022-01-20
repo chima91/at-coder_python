7a0c7c1c2ffe688f2ba35200d7995ef063bd7529
web-novice-tech <itb4769we@gmail.com>
2022-01-20 21:37:59 +0900

    「整数からなる公差1の等差数列のうち、総和がNであるものはいくつあるか」→「2N=x*y(x,yは整数)と分解した時、x=B-A+1, y=A+BとしてA,Bともに整数となるx,yの組み合わせはいくつあるか」→「2N=x*y(x,yは整数)と分解した時、x,yを2で割った余りが異なる組はいくつあるか」

---
+++ ABC/190_d.py
@@ -0,0 +1,15 @@
+# https://atcoder.jp/contests/abc190/tasks/abc190_d
+N = int(input())
+
+ans = 0
+
+i = 1
+while i*i <= 2*N:
+    if 2*N % i == 0:
+        x = i
+        y = 2*N // i
+        if x % 2 != y % 2:
+            ans += 2
+    i += 1
+
+print(ans)
