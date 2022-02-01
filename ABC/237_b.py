10a5233db9125741beb7fec08c585fae46145a54
web-novice-tech <itb4769we@gmail.com>
2022-02-01 22:26:13 +0900

    組み込み関数zip()は、複数のイテラブルオブジェクトの要素をまとめる。

--- ABC/237_b.py
+++ ABC/237_b.py
@@ -1,11 +1,21 @@
-import numpy as np
+# import numpy as np
 
+# H, W = map(int, input().split())
+
+# matrix = []
+# for _ in range(H):
+#   matrix.append(list(map(int, input().split())))
+
+# ans = np.transpose(matrix)
+# for w in range(W):
+#     print(*ans[w])
+
+
+# ↓ numpyを使わない解き方 ↓
 H, W = map(int, input().split())
+matrix = [
+  list(map(int, input().split())) for _ in range(H)
+]
 
-matrix = []
-for _ in range(H):
-  matrix.append(list(map(int, input().split())))
-
-ans = np.transpose(matrix)
-for w in range(W):
-    print(*ans[w])
+for row in zip(*matrix):
+  print(*row)
