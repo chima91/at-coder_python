89eced46481351ffb041daba5d1dad42ae5711e4
web-novice-tech <itb4769we@gmail.com>
2022-02-05 21:44:11 +0900

    コメントの追加。

--- ABC/182_c.py
+++ ABC/182_c.py
@@ -1,4 +1,5 @@
 # https://atcoder.jp/contests/abc182/tasks/abc182_c
+
 N = int(input())
 
 amari1 = False
@@ -13,25 +14,32 @@ for i in range(len(N)):
     elif keta_num % 3 == 2:
         amari2 = True
 
+# Nを3で割った余りが0の場合 → 1つも桁を消さなくて良い。
 if amari_all == 0:
     print(0)
+# Nを3で割った余りが1の場合 → 余りが1の桁が存在するかしないかで場合分け
 elif amari_all == 1:
+    # 余りが1の桁が存在する場合
     if amari1 == True:
         if len(N) <= 1:
             print(-1)
         else:
             print(1)
+    # 余りが1の桁が存在しない場合
     else:
         if len(N) <= 2:
             print(-1)
         else:
             print(2)
+# Nを3で割った余りが2の場合 → 余りが2の桁が存在するかしないかで場合分け
 elif amari_all == 2:
+    # 余りが2の桁が存在する場合
     if amari2 == True:
         if len(N) <= 1:
             print(-1)
         else:
             print(1)
+    # 余りが2の桁が存在しない場合
     else:
         if len(N) <= 2:
             print(-1)
