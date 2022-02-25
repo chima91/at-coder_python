646e511df49ee06290c212475ab3184b9c3424bb
web-novice-tech <itb4769we@gmail.com>
2022-02-26 08:17:53 +0900

    mod10をとって1か9かを判定

--- ABC/240_a.py
+++ ABC/240_a.py
@@ -1,8 +1,18 @@
+# a, b = map(int, input().split())
+
+# if a == 1 and b == 10:
+#   print('Yes')
+# elif b-a == 1:
+#   print('Yes')
+# else:
+#   print('No')
+
+
+# 別解
+# aとbの差が1か9になるか
 a, b = map(int, input().split())
 
-if a == 1 and b == 10:
-  print('Yes')
-elif b-a == 1:
+if (a-b) % 10 in [1, 9]:
   print('Yes')
 else:
   print('No')
