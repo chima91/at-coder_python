82906ccd3b0c76806e77806c6c7327466fb810b0
web-novice-tech <itb4769we@gmail.com>
2022-02-26 09:29:09 +0900

    ランレングス圧縮とは、連続して現れる符号を、繰り返しの回数を表す値に置き換える方式。可逆圧縮。

---
+++ ABC/240_d.py
@@ -0,0 +1,19 @@
+N = int(input())
+A = list(map(int, input().split()))
+
+stack = []
+cnt = 0
+
+for a in A:
+  if len(stack) != 0 and stack[-1][0] == a:
+    stack[-1][1] += 1
+  else:
+    stack.append([a, 1])
+  cnt += 1
+
+  # stackに追加した直後
+  if stack[-1][0] == stack[-1][1]:
+    cnt -= stack[-1][0]
+    stack.pop()
+
+  print(cnt)
